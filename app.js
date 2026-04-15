d3b8f928fe2688bda33e77676eb8682ac1c6d16e
shubhxm30 <shubhamnareshbhaivani@gmail.com>
2026-04-15 07:12:11 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello from NodeApp Demo 🚀 running on AWS Ubuntu!');
+  res.send('this is ci/cd works code🥳'');
 });
 
 const PORT = process.env.PORT || 3000;
