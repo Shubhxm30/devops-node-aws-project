086c4a91f8914b722fe3d3961fb9958e8af2cc68
shubhxm30 <shubhamnareshbhaivani@gmail.com>
2026-04-15 08:50:39 +0000

    Update

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 
-res.send(`
-Hello from NodeApp Demo 🚀 running on AWS Ubuntu! <br>
-this is ci/cd works code 🥳
-`);
+app.get('/', (req, res) => {
+  res.send(`
+    Hello from NodeApp Demo 🚀 running on AWS Ubuntu! <br>
+    this is ci/cd works code 🥳
+  `);
+});
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
