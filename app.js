deda7be0cb6835dad81a3023674df671e142d946
shubhxm30 <shubhamnareshbhaivani@gmail.com>
2026-05-06 06:01:25 +0000

    new chnage in app.js

--- app.js
+++ app.js
@@ -3,10 +3,42 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send(`
-    Hello from NodeApp Demo 🚀 running on AWS Ubuntu! <br>
-    this is ci/cd works code 🥳
-    this is new changes🤣
-`);
+    <html>
+      <head>
+        <title>NodeApp</title>
+        <style>
+          body {
+            font-family: Arial;
+            background: linear-gradient(to right, #1e3c72, #2a5298);
+            color: white;
+            text-align: center;
+            padding-top: 100px;
+          }
+          .card {
+            background: rgba(255,255,255,0.1);
+            padding: 30px;
+            border-radius: 15px;
+            display: inline-block;
+            box-shadow: 0 10px 30px rgba(0,0,0,0.3);
+          }
+          h1 {
+            font-size: 40px;
+          }
+          p {
+            font-size: 18px;
+            opacity: 0.9;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="card">
+          <h1>🚀 NodeApp Deployed</h1>
+          <p>Hello from AWS Linux!</p>
+          <p>CI/CD Pipeline is working successfully 🎉</p>
+        </div>
+      </body>
+    </html>
+  `);
 });
 
 const PORT = process.env.PORT || 3000;
